7a32acb767e2b1b0a6c50be048567ac28f96d3a1
Serhiy Korytskyy <korysergey@gmail.com>
2020-11-06 11:18:22 +0200

    commit MyFirstWebsite

---
+++ JSToWebsite.js
@@ -0,0 +1,33 @@
+
+
+function Serhiy(name, age) {
+alert (`Hello my name is ${name}. I am ${age} years old. It is my first website with Java Script.`);
+};
+Serhiy("Serhiy Korytskyy", 30);
+
+
+let age = prompt("Please enter your age.");
+    if(age>=18){
+        alert("Allowed");
+    }
+    else{
+        alert("Not Allowed!");
+    };
+    
+let name = prompt("Please enter your name!");
+        alert(`Hello ${name}. You are ${age} years old. Welcome to my first website.`);
+        
+    
+function show() {
+    alert("Hi I am Serhiy Korytskyy. It is my first website");
+};
+
+function sendForm(){
+    var result = confirm("Do you really to continue?");
+    if(result==true){
+        alert("Шары не бывает! Иди работай!");
+    }
+    else{
+        alert("Thanks for visiting!");
+    }
+};
